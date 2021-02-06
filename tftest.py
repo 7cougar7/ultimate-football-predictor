a658ce3561662d5fe3671836cd1eee48fe9e8724
7cougar7 <tilogarcia1@gmail.com>
2021-02-06 01:52:55 -0600

    Add database csv

--- tftest.py
+++ tftest.py
@@ -5,10 +5,8 @@ import pandas as pd
 def main():
     df_team_stats = pd.read_csv('2020 Inputs.csv')
     df_known_scores = pd.read_csv('2020 Outputs.csv')
-    new_df = []
+    new_df = None
     for idx, row in df_known_scores.iterrows():
-        if idx > 2:
-            break
         home_team_name = row['home_team']
         visiting_team_name = row['visiting_team']
 
@@ -16,7 +14,6 @@ def main():
         home_stats = home_stats.add_prefix('home_')
         home_stats = home_stats.reset_index()
         home_stats = home_stats.drop(['home_team', 'home_Unnamed: 0', 'index'], axis=1)
-        print(home_stats)
 
         visiting_stats = df_team_stats.loc[df_team_stats['team'] == visiting_team_name]
         visiting_stats = visiting_stats.add_prefix('visiting_')
@@ -29,6 +26,11 @@ def main():
                           columns=['home_score', 'visiting_score', 'win_binary'])
         df.drop(df.tail(0).index, inplace=True)
         total_stats = total_stats.join([df])
+        if new_df is None:
+            new_df = total_stats
+        else:
+            new_df = pd.concat([new_df, total_stats])
+    new_df.to_csv('finalized_db.csv')
 
 
 if __name__ == '__main__':
