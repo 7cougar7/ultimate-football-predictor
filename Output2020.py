c07dd2c74043327faa94a70c2de7bd6f9542c8e7
Marc Abdallah <marcabdallah32@gmail.com>
2021-02-06 20:24:49 -0600

    Joined all data from 2013 to 2020

--- Output2020.py
+++ Output2020.py
@@ -2,23 +2,24 @@ import pandas as pd
 
 
 def main():
-    year = '19'
-    df = pd.read_csv('data/formatted/season20' + year + '.csv')
-    df = df[1:]
-    df = df.astype({'home score': 'int32', 'visiting score': 'int32'})
-    winning_binary_list = [None]
-    for row in df.iterrows():
-        if row[1][3] > row[1][4]:
-            winning_binary_list.append(1)
-        elif row[1][3] < row[1][4]:
-            winning_binary_list.append(0)
-        else:
-            winning_binary_list.append(0.5)
-    winning_binary = pd.DataFrame(data=winning_binary_list, dtype=float, columns=['Win Binary'])
-    winning_binary = winning_binary[1:]
-    df = df.join(winning_binary)
+    years = ['18', '17', '16', '15', '14', '13']
+    for year in years:
+        df = pd.read_csv('data/formatted/season20' + year + '.csv')
+        df = df[1:]
+        df = df.astype({'home score': 'int32', 'visiting score': 'int32'})
+        winning_binary_list = [None]
+        for row in df.iterrows():
+            if row[1][3] > row[1][4]:
+                winning_binary_list.append(1)
+            elif row[1][3] < row[1][4]:
+                winning_binary_list.append(0)
+            else:
+                winning_binary_list.append(0.5)
+        winning_binary = pd.DataFrame(data=winning_binary_list, dtype=float, columns=['Win Binary'])
+        winning_binary = winning_binary[1:]
+        df = df.join(winning_binary)
 
-    df.to_csv('20' + year + ' Outputs.csv')
+        df.to_csv('20' + year + ' Outputs.csv')
 
 
 if __name__ == '__main__':
--- tftest.py
+++ tftest.py
@@ -3,35 +3,40 @@ import pandas as pd
 
 
 def main():
-    year = '19'
-    df_team_stats = pd.read_csv('20' + year + ' Inputs.csv')
-    df_known_scores = pd.read_csv('20' + year + ' Outputs.csv')
-    new_df = None
-    for idx, row in df_known_scores.iterrows():
-        home_team_name = row['home team']
-        visiting_team_name = row['visiting team']
+    years = ['20', '19', '18', '17', '16', '15', '14', '13']
+    all_inputs = pd.DataFrame()
+    for year in years:
+        df_team_stats = pd.read_csv('20' + year + ' Inputs.csv')
+        df_known_scores = pd.read_csv('20' + year + ' Outputs.csv')
+        new_df = None
+        for idx, row in df_known_scores.iterrows():
+            home_team_name = row['home team']
+            visiting_team_name = row['visiting team']
 
-        home_stats = df_team_stats.loc[df_team_stats['Team'] == home_team_name]
-        home_stats = home_stats.add_prefix('home_')
-        home_stats = home_stats.reset_index()
-        home_stats = home_stats.drop(['home_Team', 'home_Unnamed: 0', 'index'], axis=1)
+            home_stats = df_team_stats.loc[df_team_stats['Team'] == home_team_name]
+            home_stats = home_stats.add_prefix('home_')
+            home_stats = home_stats.reset_index()
+            home_stats = home_stats.drop(['home_Team', 'home_Unnamed: 0', 'index'], axis=1)
 
-        visiting_stats = df_team_stats.loc[df_team_stats['Team'] == visiting_team_name]
-        visiting_stats = visiting_stats.add_prefix('visiting_')
-        visiting_stats = visiting_stats.reset_index()
-        visiting_stats = visiting_stats.drop(['visiting_Team', 'visiting_Unnamed: 0', 'index'], axis=1)
+            visiting_stats = df_team_stats.loc[df_team_stats['Team'] == visiting_team_name]
+            visiting_stats = visiting_stats.add_prefix('visiting_')
+            visiting_stats = visiting_stats.reset_index()
+            visiting_stats = visiting_stats.drop(['visiting_Team', 'visiting_Unnamed: 0', 'index'], axis=1)
 
-        total_stats = home_stats.join([visiting_stats])
+            total_stats = home_stats.join([visiting_stats])
 
-        df = pd.DataFrame(data=[[row['home score'], row['visiting score'], row['Win Binary']]], dtype='float',
-                          columns=['home score', 'visiting score', 'Win Binary'])
-        df.drop(df.tail(0).index, inplace=True)
-        total_stats = total_stats.join([df])
-        if new_df is None:
-            new_df = total_stats
-        else:
-            new_df = pd.concat([new_df, total_stats])
-    new_df.to_csv('finalized_db20' + year + '.csv')
+            df = pd.DataFrame(data=[[row['home score'], row['visiting score'], row['Win Binary']]], dtype='float',
+                              columns=['home score', 'visiting score', 'Win Binary'])
+            df.drop(df.tail(0).index, inplace=True)
+            df['Year'] = int('20'+year)
+            total_stats = total_stats.join([df])
+            if new_df is None:
+                new_df = total_stats
+            else:
+                new_df = pd.concat([new_df, total_stats])
+        all_inputs = pd.concat([all_inputs, new_df])
+        new_df.to_csv('finalized_db20' + year + '.csv')
+    all_inputs.to_csv('all_inputs.csv')
 
 
 if __name__ == '__main__':
